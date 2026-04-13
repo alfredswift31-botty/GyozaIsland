ee5ab3e6ab9e85eca818ca2d88cea80dc678596c
AungHponeM <alfredswift31@gmail.com>
2026-04-13 10:45:34 +0100

    Initial Commit

---
+++ GyozaIsland/GyozaIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyozaIslandApp.swift
+//  GyozaIsland
+//
+//  Created by Aung Hpone Moe on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct GyozaIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GyozaIslandTests/GyozaIslandTests.swift
@@ -0,0 +1,17 @@
+//
+//  GyozaIslandTests.swift
+//  GyozaIslandTests
+//
+//  Created by Aung Hpone Moe on 13/04/2026.
+//
+
+import Testing
+@testable import GyozaIsland
+
+struct GyozaIslandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
